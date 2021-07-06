c2e7ff3847581a115fdcdb7723f34dd574bc4d2d
Chandni <chandniasnani.ca@gmail.com>
2021-07-06 19:43:25 +0530

    [Modify] Assignment2

--- Assignment2/main.py
+++ Assignment2/main.py
@@ -5,21 +5,28 @@ from problems.implementation.prime import Prime
 
 
 if __name__ == "__main__":
-    
-    print("enter 1 for fibonacci, 2 for prime , 3 for armstrong")
-    problem = int(input())
-    while problem not in range(1,4):
-        print("Please provide valid problem")
+
+
+    try:
         print("enter 1 for fibonacci, 2 for prime , 3 for armstrong")
         problem = int(input())
-    print("enter number")
-    number = int(input())
+        while problem not in range(1,4):
+            print("Please provide valid problem")
+            print("enter 1 for fibonacci, 2 for prime , 3 for armstrong")
+            problem = int(input())
+        print("enter number")
+        number = int(input())
 
 
-    d = {
-    1: Fibonacci(number).run(),
-    2: Prime(number).run(),
-    3: Armstrong(number).run()
-}
+        d = {
+        1: Fibonacci(number).run(),
+        2: Prime(number).run(),
+        3: Armstrong(number).run()
+    }
     
-    print(d[problem])
+        print(d[problem])
+
+
+    except Exception as e:
+        print("Error : please enter valid input in integers")
+
--- Assignment2/problems/implementation/fibonacci.py
+++ Assignment2/problems/implementation/fibonacci.py
@@ -3,9 +3,10 @@ from base import Base
 class Fibonacci(Base):
 
     def run(self):
+        lst = [1,2]
         if self.number == 0:
             return 0
-        elif self.number == 1 or self.number == 2:
+        elif self.number in lst:
             return 1
         else:
             return Fibonacci(self.number-1).run() + Fibonacci(self.number-2).run()
